7d63b2f471db6995d20214b9a950ef0cc7881605
bagohart <bagohart@gmx.de>
2021-01-07 16:17:26 +0100

    added some experiments on thinking with types ch. 3

--- thinking_with_types/ch3/ch3.hs
+++ thinking_with_types/ch3/ch3.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE InstanceSigs, DeriveFunctor, ScopedTypeVariables #-}
 
 newtype T1 a = T1 (Int -> a)
 
@@ -77,3 +77,62 @@ instance Functor T5 where
 -- Example: Either, (,)
 -- contravariant in its first, covariant in its second argument: profunctor
 -- Example: (->)
+
+-- ... if all of this is correct, I should be able to predict and construct even more Functor instances in a similar way:
+newtype T6 a = T6 ((a -> Int) -> a)
+-- this is a combination of T1 and T5...
+instance Functor T6 where 
+    fmap :: (a -> b) -> T6 a -> T6 b
+    fmap ab (T6 aia) = T6 $ \bi -> ab (aia (bi . ab))
+
+newtype T7 a = T7 (((Int -> a) -> Int) -> Int)
+-- this is like T4, but nested in yet another function, so it should be like + * - * - = +
+-- ((Int -> a) -> Int) -> Int
+--          +
+--                   -
+--                           -
+-- automatic deriving works, so this should be possible manually...
+runT7 :: T7 a -> (((Int -> a) -> Int) -> Int)
+runT7 (T7 x) = x
+
+instance Functor T7 where 
+    fmap :: (a -> b) -> T7 a -> T7 b
+    fmap ab (T7 iaii) = T7 $ \ibi -> iaii $ (\ia -> ibi $ ab . ia)
+        -- I need to produce an Int.
+        -- I need to apply either iaii or ibi to produce this Int.
+        -- Let's try aiaa first:      iaii :: ((Int -> a) -> Int) -> Int
+        -- So iaii needs an ((Int -> a) -> Int), then it can produce an Int for us.
+        -- And what I have is an ibi :: (Int -> b) -> Int which hopefully matches
+        -- So I'll need to turn the ibi into an iai using ab:
+        -- ibi :: (Int -> b) -> Int
+        -- + ab :: a -> b
+        -- ???
+        -- ~> iai :: (Int -> a) -> Int
+        -- ??? = \ia -> ibi $ ab . ia
+        --
+        -- ... ok, so that worked, though I won't pretend to be able to hold all of that in my head at once :)
+
+-- since it was so fun, let's level up this again...
+
+-- this is like T7, except now it's becoming ridiculous...
+newtype T8 a = T8 (((((Int -> a) -> Int) -> Int) -> Int) -> Int) -- deriving Functor
+
+instance Functor T8 where 
+    fmap :: (a -> b) -> T8 a -> T8 b
+    fmap ab (T8 iaiiii) = T8 $ \ibiii -> iaiiii $ (\iaii -> ibiii (runT7 (fmap ab (T7 iaii))))
+-- this is too convoluted to keep it in my hand but just looking at the types it becomes obvious
+-- that this reuses T7...
+
+-- previous attempt that doesn't work because a !~ a1 or something, even with ScopedTypeVariables o_O
+-- instance Functor T8 where 
+--     fmap :: (a -> b) -> T8 a -> T8 b
+--     fmap ab (T8 iaiiii) = T8 $ \ibiii -> 
+--         let lol_iaiii :: (((Int -> a) -> Int) -> Int) -> Int
+--             lol_iaiii = \iaii -> ibiii (runT7 (fmap ab (T7 iaii)))
+--             -- ibiii (lol_ibii
+--          in iaiiii $ lol_iaiii
+    -- ?1 :: iaiii
+    -- => from ibiii and ab create iaiii somehow
+    -- ?1 = (\iaii
+
+    -- fmap ab (T7 iaii) = T7 $ \ibi -> iaii $ (\ia -> ibi $ ab . ia)
